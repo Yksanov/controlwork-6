f0523941bddd147aed1473185e8f39526a1490cd
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-05 12:06:08 +0600

    create class ToDoList and added properties

---
+++ ControlWork6/ToDoList.cs
@@ -0,0 +1,10 @@
+namespace ControlWork6;
+
+public class ToDoList
+{
+    public int Id { get; set; }
+    public string Header { get; set; }
+    public string Username { get; set; }
+    public string CreateDate { get; set; }
+    public bool Status { get; set; }
+}
